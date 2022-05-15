aa8731c97b501ae79f5a5f429cbfad0672b12cfb
justingracie <j.m.gracie@gmail.com>
2022-05-15 12:46:47 -0600

    hitting API, displaying department names

--- src/pages/Collection.jsx
+++ src/pages/Collection.jsx
@@ -1,6 +1,35 @@
+import React from "react"
+import { useState, useEffect} from 'react'
+
+
 const Collection = ()=>{
+    const URL ='https://collectionapi.metmuseum.org/public/collection/v1/departments'
+    const[art, setArt] = useState([])
+
+    useEffect(() => {
+        const getArt = async () => {
+            try {
+                const response = await fetch(URL)
+                const data = await response.json()
+                setArt(data.departments)
+                console.log(art)
+            } catch (error) {
+                console.log('there has been an error', error)
+            }
+        }
+        getArt()
+    }, [])
+
     return(
-        <h1>Collection Page Goes Here</h1>
+        <div>
+            {art.map((department, idx) =>{
+                return(
+                    <ul key={idx}>
+                        <li>{department.displayName}</li>
+                    </ul>
+                )
+            })}
+        </div>
     )
 }
 
