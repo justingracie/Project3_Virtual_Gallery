2277230d9a20c4a5a994bb02d61eb2ad7a569d10
justingracie <j.m.gracie@gmail.com>
2022-05-17 15:19:55 -0600

    afternoon commit, still working on homepage carousel

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,9 @@ const Home = ()=>{
         <div>
             <h1>Home Page Goes Here</h1>
                 <ul className="gallery js-gallery">
-                    <li className="homeArt"> <img className="homeImage" src="https://www.artic.edu/iiif/2/86cb98bf-0279-86ce-3995-fa42e6d76be2/full/843,/0/default.jpg"></img></li>
-                    <li className="homeArt"> <img className="homeImage" src="https://www.artic.edu/iiif/2/11cfc829-cf72-3b5f-c59f-44357721d09e/full/843,/0/default.jpg"></img></li>
-                    <li className="homeArt"> <img className="homeImage" src="https://www.artic.edu/iiif/2/b7b7ecd0-5a50-b89a-c72a-d435a1957c73/full/843,/0/default.jpg"></img></li>
+                    <li className="homeArt js-gallery-item"> <img className="homeImage" src="https://www.artic.edu/iiif/2/86cb98bf-0279-86ce-3995-fa42e6d76be2/full/843,/0/default.jpg"></img></li>
+                    <li className="homeArt js-gallery-item" > <img className="homeImage" src="https://www.artic.edu/iiif/2/11cfc829-cf72-3b5f-c59f-44357721d09e/full/843,/0/default.jpg"></img></li>
+                    <li className="homeArt js-gallery-item"> <img className="homeImage" src="https://www.artic.edu/iiif/2/b7b7ecd0-5a50-b89a-c72a-d435a1957c73/full/843,/0/default.jpg"></img></li>
                     {/* <li className="homeArt" style="background-image: url('https://www.artic.edu/iiif/2/11cfc829-cf72-3b5f-c59f-44357721d09e/full/843,/0/default.jpg')"></li> */}
                     {/* <li className="homeArt" style="background-image: url('https://www.artic.edu/iiif/2/b7b7ecd0-5a50-b89a-c72a-d435a1957c73/full/843,/0/default.jpg')"></li> */}
 
