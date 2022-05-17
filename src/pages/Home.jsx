0e3cdab32ae720c2dcca450ac2a1abf07c99f2ab
justingracie <j.m.gracie@gmail.com>
2022-05-17 13:17:16 -0600

    setting up homepage images for slideshow

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,16 @@
 const Home = ()=>{
     return(
-        <h1>Home Page Goes Here</h1>
+        <div>
+            <h1>Home Page Goes Here</h1>
+                <ul className="gallery js-gallery">
+                    <li className="homeArt"> <img src="https://www.artic.edu/iiif/2/86cb98bf-0279-86ce-3995-fa42e6d76be2/full/843,/0/default.jpg"></img></li>
+                    <li className="homeArt"> <img src="https://www.artic.edu/iiif/2/11cfc829-cf72-3b5f-c59f-44357721d09e/full/843,/0/default.jpg"></img></li>
+                    <li className="homeArt"> <img src="https://www.artic.edu/iiif/2/b7b7ecd0-5a50-b89a-c72a-d435a1957c73/full/843,/0/default.jpg"></img></li>
+                    {/* <li className="homeArt" style="background-image: url('https://www.artic.edu/iiif/2/11cfc829-cf72-3b5f-c59f-44357721d09e/full/843,/0/default.jpg')"></li> */}
+                    {/* <li className="homeArt" style="background-image: url('https://www.artic.edu/iiif/2/b7b7ecd0-5a50-b89a-c72a-d435a1957c73/full/843,/0/default.jpg')"></li> */}
+
+                </ul>
+        </div>
     )
 }
 
