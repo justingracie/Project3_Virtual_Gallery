e2b7f466668c357caf14289ce1bac8486b066a01
justingracie <j.m.gracie@gmail.com>
2022-05-18 13:12:58 -0600

    Linked carousel images to exhibits

--- src/components/CarouselComponent.jsx
+++ src/components/CarouselComponent.jsx
@@ -1,18 +1,31 @@
 import { Carousel } from 'react-responsive-carousel'
 import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Link } from 'react-router-dom'
  
 const CarouselComponent = () => {
     return(
     <div className='carousel-wrapper'>
         <Carousel infiniteLoop useKeyboardArrows autoPlay interval={6000}>
             <div>
-                <img className='homeArt' src="../Carousel_1.jpeg" alt="City of Drawers" />
+                <Link to='/collectionDali'>
+                    <div>
+                        <img className='homeArt' src="../sadness.jpeg" alt="City of Drawers" />
+                    </div>
+                </Link>
             </div>
             <div>
-                <img className='homeArt' src="../Carousel_2.jpeg" alt="Sketchbook No. 2 (L'Estaque)" />
+                <Link to='/collectionCezanne'>
+                    <div>
+                        <img className='homeArt' src="../Skulls.jpeg" alt="Sketchbook No. 2 (L'Estaque)" />
+                    </div>
+                </Link>
             </div>
             <div>
-                <img className='homeArt' src="../Carousel_3.jpeg" alt="Nude under a Pine Tree" />
+                <Link to='/collectionPicasso'>
+                    <div>
+                        <img className='homeArt' src="../Still.jpeg" alt="Nude under a Pine Tree" />
+                    </div>
+                </Link>
             </div>
         </Carousel>
     </div>
